e40d19fcda369390038016c902b9937b111951d6
unknown <marinecorbel@yahoo.fr>
2021-10-31 21:04:52 +0100

    formulaire actif

--- javascript/index.js
+++ javascript/index.js
@@ -5,19 +5,35 @@ $.addEventListener("DOMContentLoaded", () => {
   const body = $.querySelector("body");
   const header = $.querySelector("header");
 
-  // Add bottom border on header when scrolling on axe Y
-
+  // Afficher le formulaire sur le boutton connectez-vous
   $.querySelector(".formbutton").addEventListener("click", () => {
     // Display modal
     modal.classList.remove("hidden");
     // Disable scroll
     body.classList.add("disable");
   });
+
+  // fermeture du formulaire
+
   $.querySelector(".icon-times").addEventListener("click", () => {
     // Hide modal
     modal.classList.add("hidden");
     // Enable scroll
     body.classList.remove("disable");
+  });
+
+  // Récupérer les infos du formulaire
+  $.querySelector("#Myform").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
+    const response = await axios.post("http://localhost:3000/form", data);
+    console.log(response);
   });
 
   console.log("page chargée");
